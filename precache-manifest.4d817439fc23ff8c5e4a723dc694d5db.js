557f4f8925caedea94792aace46b7cb1b5a39339
Reid Mitchell <reidmitchell1@gmail.com>
2020-03-31 01:58:25 -0700

    Updates

---
+++ precache-manifest.4d817439fc23ff8c5e4a723dc694d5db.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ef7df0de35aad08c7486cc95e80a18d",
+    "url": "/board-game/index.html"
+  },
+  {
+    "revision": "62c3f5e59b8a9715ead4",
+    "url": "/board-game/static/css/main.4f6169ce.chunk.css"
+  },
+  {
+    "revision": "65f1e1322821af927ce2",
+    "url": "/board-game/static/js/2.e8fe49d2.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/board-game/static/js/2.e8fe49d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "62c3f5e59b8a9715ead4",
+    "url": "/board-game/static/js/main.8276ed5d.chunk.js"
+  },
+  {
+    "revision": "b306f7337ab6e04cb191",
+    "url": "/board-game/static/js/runtime-main.cfef7392.js"
+  }
+]);
